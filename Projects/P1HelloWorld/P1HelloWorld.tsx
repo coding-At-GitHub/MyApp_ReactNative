1e5beb24fd1062f489f3d945e488c47fc67e8800
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2024-02-16 05:40:04 +0000

    hello world

---
+++ Projects/P1HelloWorld/P1HelloWorld.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function P1HelloWorld() {
+    return (
+        <View>
+          <Text>Hello React Native</Text>
+        </View>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+
+    });
+    
