822aae6b34671979a5ea7c823e88378e9686ba8e
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2024-02-19 13:01:25 +0530

    importing both component files

--- Projects/P2BasicUI/P2BasicUiLandingPage.tsx
+++ Projects/P2BasicUI/P2BasicUiLandingPage.tsx
@@ -1,5 +1,7 @@
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import FlatCards from './Components/FlatCards/FlatCards'
+import ScrollCards from './Components/Scrollcards/ScrollCards'
 
 export default function P2BasicUiLandingPage() {
     return (
@@ -8,6 +10,8 @@ export default function P2BasicUiLandingPage() {
                 <View style={styles.titleBox}>
                     <Text style={styles.title}>My First Project</Text>
                 </View>
+                <FlatCards />
+                <ScrollCards />
             </ScrollView>
         </SafeAreaView>
     )
@@ -25,5 +29,10 @@ const styles = StyleSheet.create({
         fontWeight: '700',
         borderBottomWidth: 6,
         borderBottomColor: '#c0c0c0',
+
+        // underline: true,
+        // flex:1,
+        // alignItems: 'center',
+        // justifyContent: 'center',
     }
 })
